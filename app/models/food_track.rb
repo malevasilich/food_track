5fef3a8715632a31b0ce1814058f942823788f2c
malevasilich <malevasilich@gmail.com>
2013-04-10 05:30:25 +0000

    fat part in cluster

--- app/models/food_track.rb
+++ app/models/food_track.rb
@@ -15,4 +15,28 @@ class FoodTrack < ActiveRecord::Base
   	self.food.fat/100*self.weight
   end
 
+  def load_cluster
+    dt = 0
+    dt1 = self.date
+    while dt1!=dt do
+      dt = dt1
+      dt1 = FoodTrack.where(:date => dt-60*60..dt).order('date').first.date
+    end
+    min_cluster_date = dt
+
+    dt = 0
+    dt1 = self.date
+    while dt1!=dt do
+      dt = dt1
+      dt1 = FoodTrack.where(:date => dt..dt+60*60).order('date').last.date
+    end
+    max_cluster_date = dt
+
+    FoodTrack.where(:date => min_cluster_date..max_cluster_date)
+  end
+
+  def cluster_kcals
+    load_cluster.sum(&:kcals)
+  end
+
 end
