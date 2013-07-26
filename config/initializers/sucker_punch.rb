e154c29d6e49ff2eae13b03269e0c76867a52934
malevasilich <malevasilich@gmail.com>
2013-07-26 08:50:40 +0000

    sp config?

--- config/initializers/sucker_punch.rb
+++ config/initializers/sucker_punch.rb
@@ -1,5 +1,5 @@
-SuckerPunch.config do
-  queue name: :create_queue, worker: CreateWorker, workers: 2
-  #queue name: :update_queue, worker: UpdateWorker, workers: 2
-  #queue name: :delete_queue, worker: DeleteWorker, workers: 2
-end
+# SuckerPunch.config do
+#   queue name: :create_queue, worker: CreateWorker, workers: 2
+#   queue name: :update_queue, worker: UpdateWorker, workers: 2
+#   queue name: :delete_queue, worker: DeleteWorker, workers: 2
+# end
