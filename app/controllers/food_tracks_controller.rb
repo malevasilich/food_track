37949fdab8f015a28a3edbadceb330ed429457a9
malevasilich <malevasilich@gmail.com>
2016-04-07 12:51:00 +0600

    c14

--- app/controllers/food_tracks_controller.rb
+++ app/controllers/food_tracks_controller.rb
@@ -45,6 +45,7 @@ class FoodTracksController < ApplicationController
         if ft.fitbit_logid.nil?
           CreateWorker.new.async.perform(ft) if !Rails.env.test?
           #heroku?
+#c14?
         end
       end
 
